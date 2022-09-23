4f2b968f21ad5f84bc21fab3769a45c67cd190ca
cf384 <cf384@M3>
2022-09-23 17:13:29 -0600

    Agregar archivos de proyecto.

---
+++ OperacionDiv4/Form2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperacionDiv4
+{
+    internal class Form2
+    {
+        public partial class Form1 : Form
+        {
+            public Form1()
+            {
+                InitializeComponent();
+            }
+
+            private void button1_Click(object sender, EventArgs e)
+            {
+                cldivision cldivision = new cldivision(double.Parse(textBox1.Text), double.Parse(textBox2.Text));
+                textBox3.Text = cldivision.dividir().ToString();
+            }
+        }
+    }
+
+}
